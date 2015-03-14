09d035f5c4fc570567f053e874d00de70444cfb4
nyuscps <nyuscps@ww335b-wk03.lab.scps.nyu.edu>
2015-03-14 15:51:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nyuscps/touch/TouchView.java
@@ -0,0 +1,90 @@
+package com.example.nyuscps.touch;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nyuscps on 3/7/15.
+ */
+public class TouchView extends View {
+    //private PointF p = new PointF();	//holds 2 floats
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private boolean first = true;
+    //private int i = 0;
+    //private PointF[] arr = new PointF[10];
+    ArrayList<PointF> points = new ArrayList<PointF>();
+    //TRY ARRAY LIST WHICH CAN CHANGE SIZE ON THE GO
+
+    public TouchView(Context context) {
+        super(context);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(135f);
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //arr[i] = new PointF(event.getX(), event.getY());
+                        points.add(new PointF(event.getX(), event.getY()));
+                        //i++;
+                        //Put center of circle where finger touched.
+                        //p.set(event.getX(), event.getY());
+                        invalidate();    //call onDraw method of TouchView
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        final int width = getWidth();
+        final int height = getHeight();
+        float radius = .3f * Math.min(width/2, height/2);
+
+        if (first)
+        {
+            //p.set(width/2, height/2);
+            first = false;
+
+        }
+
+        canvas.drawColor(Color.WHITE);	//background
+        //canvas.drawCircle(p.x, p.y, radius, paint);
+
+        // Draws patton and then a circle with a thick white line that covers
+        // part of the image.
+        Resources resources = getResources();
+        Bitmap bitMap = BitmapFactory.decodeResource(resources, R.mipmap.patton);
+        int w = bitMap.getWidth();
+        int h = bitMap.getHeight();
+        //coordinates of upper left corner of BitMap, paint is null
+        //canvas.drawBitmap(bitMap, p.x - w/2, p.y - h/2, null);
+        //canvas.drawCircle(p.x, p.y, radius, paint);
+
+        for (int j = 0; j<points.size(); j++)
+        {
+            PointF p = points.get(j); //arr[j];
+            canvas.drawBitmap(bitMap, p.x - w/2, p.y - h/2, null);
+            canvas.drawCircle(p.x, p.y, radius, paint);
+        }
+    }
+}
